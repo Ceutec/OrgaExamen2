3f007aa1744892aa8ad6ddabe85b6df77ad22b5a
Turupawn <ahmed.hn.43@gmail.com>
2014-12-09 08:56:29 -0600

    d

---
+++ main.cpp
@@ -0,0 +1,34 @@
+// queue::push/pop
+#include "Evaluador.h"
+#include "NodoAVL.h"
+#include <iostream>
+#include "tinyxml2.h"
+#include <iostream>
+#include <list>
+using namespace std;
+using namespace tinyxml2;
+
+//Devuelve la cantidad de usuarios que tiene 18 anios o mas dado el nombre de un archivo XML
+int contarMayoresDeEdadXML(string nombre_archivo)
+{
+    return -1;
+}
+
+//Devuelve el numero anterior del nodo raiz de un Arbol dado
+int obtenerNumeroAnterior(NodoAVL* raiz)
+{
+    return -1;
+}
+
+//Devuelve el numero siguiente del nodo raiz de un Arbol dado
+int obtenerNumeroSiguiente(NodoAVL* raiz)
+{
+    return -1;
+}
+
+int main ()
+{
+    //Funcion evaluadora
+    evaluar();
+    return 0;
+}
